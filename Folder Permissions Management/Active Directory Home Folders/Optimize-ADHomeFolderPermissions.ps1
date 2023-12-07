7a5ec525553802f775a86f312b7d503e2af3bbc8
Nicholas <153095516+NicJames2378@users.noreply.github.com>
2023-12-07 18:10:08 -0500

    Added AD Home Directory Scripts

---
+++ Folder Permissions Management/Active Directory Home Folders/Optimize-ADHomeFolderPermissions.ps1
@@ -0,0 +1,73 @@
+﻿param (
+    [Parameter(Mandatory, Position = 0)]
+    [string] $ADHomeFolderPath,
+
+    [Parameter(Position = 1)]
+    [string] $OwnerName
+)
+
+
+$ownerObject = $null
+$ErrorActionPreference = "Stop"
+
+
+# Test ACL capabilities
+if (-not ([string]::IsNullOrWhiteSpace($OwnerName))) {
+    "OwnerName is defined. Will attempt to set new folder owners."
+    
+    try {
+        $ownerObject = New-Object System.Security.Principal.Ntaccount($OwnerName)
+
+        # This is safe because we never save the ACL back!
+        $tempAcl = Get-Acl 'C:\Windows\System32\cmd.exe'
+        $tempAcl.SetOwner($ownerObject)
+
+        "Succeeded testing ACL capabilities. Will attempt to change owners!"
+    } catch {
+        Write-Warning "Failed to test setting owner permissions. Aborting!"
+        continue
+    }
+}
+
+
+Get-ChildItem $ADHomeFolderPath -Directory | ForEach-Object {
+    $hf = $_
+    $folderPath = $hf.FullName
+    $acl = Get-Acl $folderPath
+
+    # Set Owner
+    if ($null -ne $ownerObject) {
+        $acl.SetOwner($ownerObject)
+    }
+
+
+    # Lookup username in AD
+    try {
+        $adUser = Get-ADUser -Identity $hf.Name
+        $user = $adUser.UserPrincipalName
+    } catch {
+        Write-Warning "BAD_USER [$($hf.Name)] - Skipping path [$($hf.FullName)]"
+        return
+    }
+
+
+    # Remove all ACEs
+    $acesRemove = $acl.Access | ?{ $_.IsInherited -eq $false }
+    foreach ($ace in $acesRemove) {
+        $acl.RemoveAccessRuleAll($ace)
+    }
+    Set-Acl -AclObject $acl $folderPath
+
+
+    # Set new perms
+    $inheritanceFlag = [System.Security.AccessControl.InheritanceFlags]::ContainerInherit -bor [System.Security.AccessControl.InheritanceFlags]::ObjectInherit
+    $propagationFlag = [System.Security.AccessControl.PropagationFlags]::InheritOnly
+    $objType = [System.Security.AccessControl.AccessControlType]::Allow 
+    $permission = $user, "Modify", $inheritanceFlag, $propagationFlag, $objType
+    $aceNew = New-Object System.Security.AccessControl.FileSystemAccessRule $permission
+    $acl.SetAccessRule($aceNew)
+    Set-Acl $folderPath $acl
+
+
+    "Fully processed $folderPath"
+}
---
+++ Folder Permissions Management/Active Directory Home Folders/Test-ADHomeFolders.ps1
@@ -0,0 +1,55 @@
+﻿param (
+    [Parameter(ParameterSetName = "Audit", Position = 0)]
+    [string] $ADHomeFolderPath,
+
+    [Parameter(ParameterSetName = "Audit", Position = 1)]
+    [int] $UnusedInDays = -1,
+
+    [Parameter(ParameterSetName = "Audit", Position = 2)]
+    [int] $AccountDisabledForDays = -1
+)
+
+$allHomeDirectories = [System.Collections.Generic.List[PSCustomObject]]::new()
+
+Get-ADUser -Properties HomeDirectory -Filter * | Where { $null -ne $_.HomeDirectory } | Select SamAccountName,UserPrincipalName,HomeDirectory,Enabled | 
+ForEach-Object {
+	$folder = $_.HomeDirectory.Split($_.HomeDirectory[0])[-1]
+	$name = $_.SamAccountName
+	if ($folder -ne $name) {
+		"Home Mismatch: $($_.UserPrincipalName): $folder"
+	}
+    
+    $allHomeDirectories.Add([PSCustomObject]@{
+        Path = $_.HomeDirectory
+        AssociatedAccountEnabled = $_.Enabled
+    })
+}
+
+
+"Home directory reachability report. Note that this could be invalid if the account scanning does not have permissions."
+"Scanning as user [$($env:USERDOMAIN)\$($env:USERNAME)]"
+
+for ($i = 0; $i -lt $allHomeDirectories.Count; $i++) {
+    $dirPath = $allHomeDirectories[$i].Path
+
+    # Report connectivity (imperfect!)
+    $reachable = Test-Path $dirPath
+    $allHomeDirectories[$i] | Add-Member -MemberType NoteProperty -Name Reachable -Value (Test-Path $dirPath)
+
+    # If able to connect, check stats on the folder
+    if ($reachable) {
+        $item = Get-Item -LiteralPath $dirPath
+        $daysSinceLastAccess = [System.Math]::Round(((Get-Date) - ((Get-Item -LiteralPath $allHomeDirectories[$i].Path).LastAccessTime)).TotalDays)
+        $allHomeDirectories[$i] | Add-Member -MemberType NoteProperty -Name DaysSinceLastAccessed -Value $daysSinceLastAccess
+    } else {
+        $allHomeDirectories[$i] | Add-Member -MemberType NoteProperty -Name DaysSinceLastAccessed -Value '-'
+    }
+
+    # Check if the path seems to be a standard path
+    $counts = $allHomeDirectories | % { $_.Path.Substring(0, $_.Path.LastIndexOf($_.Path[0])).ToLower() -replace '/', '\' } | Group-Object
+    $pathIsMajority = $o[0].Count -gt ($allHomeDirectories.Count * 0.7)
+
+    $allHomeDirectories[$i] | Add-Member -MemberType NoteProperty -Name IsCommonPath -Value $pathIsMajority
+}
+
+$allHomeDirectories
